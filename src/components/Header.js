be5bbee0b39443713e0418c25e4c799e1d1775d4
hs-gautam <gautam@genero.jp>
2018-12-24 23:22:45 +0530

    Update image.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Footer from './Footer'
-import avatar from '../assets/images/doruparu.jpg'
+import avatar from '../assets/images/gautam.png'
 
 class Header extends React.Component {
     render() {
